36321f2af7843540c446509c89b94be3273e3b8c
AmandaRami <amanda.raim@sptech.school>
2024-10-18 00:35:05 -0300

    tela de cadastro e login

--- earthmoon/src/routes.js
+++ earthmoon/src/routes.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Accessories from './pages/Accessories/Accessories';
 import Products from './pages/Products/Products';
 import Home from './pages/Home/Home';
+import Cadastro from './pages/Cadastro/Cadastro'; 
+import SidePanelLogin from './components/SidePanel/SidePanel';
+
 
 const RoutesComponent = () => {
   return (
@@ -11,6 +14,8 @@ const RoutesComponent = () => {
         <Route path="/" element={<Home />} />
         <Route path="/acessorios" element={<Accessories />} />
         <Route path="/produtos" element={<Products />} />
+        <Route path="/register" element={<Cadastro />} />
+        <Route path="/login" element={<SidePanelLogin isOpen={true} onClose={() => {}} />} /> 
       </Routes>
     </Router>
   );
