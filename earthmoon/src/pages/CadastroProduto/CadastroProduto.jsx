e5369c6be39cc15c43043444b836dd0f08a6e711
RyanMirandaBarbosa <ryan.barbosa@sptech.school>
2024-10-19 01:37:17 -0300

    Adicionando pagina de cadastrar produtos

---
+++ earthmoon/src/pages/CadastroProduto/CadastroProduto.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import ProductsRegister from '../../components/ProductsRegister/ProductsRegister';
+
+const CadastroProduto = () => {
+  return (
+    <div>
+      <Navbar />
+      <ProductsRegister />
+    </div>
+  );
+};
+
+export default CadastroProduto;
--- earthmoon/src/routes.js
+++ earthmoon/src/routes.js
@@ -5,7 +5,7 @@ import Products from './pages/Products/Products';
 import Home from './pages/Home/Home';
 import Cadastro from './pages/Cadastro/Cadastro'; 
 import SidePanelLogin from './components/SidePanel/SidePanel';
-
+import CadastroProduto from './pages/CadastroProduto/CadastroProduto';  // Importando a nova página de cadastro de produto
 
 const RoutesComponent = () => {
   return (
@@ -16,6 +16,7 @@ const RoutesComponent = () => {
         <Route path="/produtos" element={<Products />} />
         <Route path="/register" element={<Cadastro />} />
         <Route path="/login" element={<SidePanelLogin isOpen={true} onClose={() => {}} />} /> 
+        <Route path="/cadastro-produto" element={<CadastroProduto />} />  {/* Nova rota */}
       </Routes>
     </Router>
   );
